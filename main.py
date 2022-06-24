db14da430d25dda062b30456da62eb314b95c57e
Vladyslav Ziberov <siegbertov@gmail.com>
2022-06-24 17:12:29 +0300

    Changed Strategy Implementation (with dict)

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# from natural_selection import Evolution
-#
-#
+from natural_selection import Evolution
+
+
 # my_evolution = Evolution()
-#
-#
+
 # my_evolution.evolve()
 
-
 def main():
-    cards = ('A', '10', '9', '8', '7', '6', '5', '4', '3', '2')
-    inputs = tuple([f"{c1}_{c2}_{c3}" for c1 in cards for c2 in cards[cards.index(c1):] for c3 in cards])
-    print(len(inputs))
+    pass
 
 
 if __name__ == "__main__":
--- natural_selection/strategy.py
+++ natural_selection/strategy.py
@@ -1,21 +1,25 @@
-from .blackjack import Game
-
-
-cards = ('A', '10', '9', '8', '7', '6', '5', '4', '3', '2')
-inputs = [f"{c1}_{c2}_{c3}" for c1 in cards for c2 in cards[cards.index(c1):] for c3 in cards]
-inputs.append('threshold')
-inputs.append('fitness_score')
+from blackjack import Game, ActionSpace
+from random import choice
 
 
 class Strategy:
-    __slots__ = tuple(inputs)
-
     def __init__(self):
+        self.decisions = {}
+        self.fitness_score = None
         self._create()
 
     def _create(self):
-        # TODO: implement _create()
-        pass
+        cards = ('A', '10', '9', '8', '7', '6', '5', '4', '3', '2')
+
+        for dec in [f"{c1}_{c2}_{c3}" for c1 in cards for c2 in cards[cards.index(c1):] for c3 in cards]:
+            if len(set(dec.split("_")[:-1])) == 1:
+                self.decisions[dec] = choice([ActionSpace.SPLIT, ActionSpace.HIT, ActionSpace.STAND])
+            else:
+                self.decisions[dec] = choice([ActionSpace.HIT, ActionSpace.STAND])
+        self.decisions['threshold'] = choice([16, 17, 18])
+
+    def __str__(self):
+        return str(self.decisions)
 
     def fitness(self, number_of_games=1000, fitness_goal='win'):
         """fitness_goal => {'win', 'lose', 'draw'}"""
@@ -32,3 +36,12 @@ class Strategy:
     def crossover(self, other):
         # TODO: implement crossover()
         pass
+
+
+def main():
+    my_strategy = Strategy()
+    print(my_strategy)
+
+
+if __name__ == "__main__":
+    main()
